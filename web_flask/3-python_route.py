2d39a36bf45a257e2f99bdd1a16e31660ef00fba
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-27 12:48:55 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" starts a Flask web application. """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays Hello Hbnb>"""
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB."""
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Displays C followed by the value of the text variabl."""
+
+    replace_underscore = text.replace('_', ' ')
+    return f"C {replace_underscore}"
+
+
+@app.route('/python', defaults='text': 'is cool')
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """display “Python ”, followed by the value of the text variable."""
+
+    replace_text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
