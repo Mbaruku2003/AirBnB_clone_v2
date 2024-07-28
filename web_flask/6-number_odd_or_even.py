40288af9357557619c59a5642a7680a8636a2c6a
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-28 10:47:37 -0700

    updated number

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -62,5 +62,7 @@ def number_odd_or_even(n):
             "evenodd": evenodd
             }
     return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
