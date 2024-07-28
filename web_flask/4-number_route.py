5dd276a23140af0bc818ae7022b964796adea94e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-07-28 07:12:46 -0700

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -37,12 +37,11 @@ def python(text):
     return 'Python {}'.format(replace_text)
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<n:int>', strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer."""
 
-    if isinstance(n, int):
-        return 'n is a number'
+    return '{} is a number'.format(n)
 
 
 if __name__ == "__main__":
